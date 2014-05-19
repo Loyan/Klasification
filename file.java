62856453ba544590f893f3f4328f7783640df5e5
Loyan <loyan93@mail.ru>
2014-05-19 23:03:33 +0300

    Update file.java

--- file.java
+++ file.java
@@ -9,7 +9,8 @@ public class file {
 	static String [][] m = new String [1][1255];
 	static Scanner scn;
 
-public static void main(String args[]){
+public static void main(String args[])
+{
 	openFile();
 	readFile();
 	out();
@@ -21,23 +22,23 @@ private static void out() {
 	System.out.print(m[row][col]+ "   ");
 }
 System.out.println();
+ }
 }
-}
-
+		
 private static void readFile() {
 	while(scn.hasNext()){
 	for(int row=0;row<m.length;row++){
 	for(int col=0;col<m[row].length;col++){
 	m[row][col] = scn.next();
 	
-}
-}
-}
+    }
+   }
+  }
 }
 
 private static void openFile() {
 	try{
 	scn = new Scanner(new File("res//UX.txt"));
 	} catch(Exception e){JOptionPane.showMessageDialog(null, "Файл не найден");}
-}
+ }
 }
