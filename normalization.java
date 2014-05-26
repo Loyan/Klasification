dba0d4a5c4926109bf7118637ae069c784ad1190
Loyan <loyan93@mail.ru>
2014-05-26 23:40:35 +0300

    Update normalization.java

--- normalization.java
+++ normalization.java
@@ -1,22 +1,77 @@
 import java.lang.Math;
+import java.lang.*;
+import java.io.*;
+import javax.swing.*;
 
-public class normalization extends MyFileClass {
-	
-   static double a;
-
- public static void main(String args[])
+public class normalization extends MyFileClass 
 {
+	
+   static double[] g = new double[1254];
+   static double h;
+   static double j;
+   static double k;
+   static int col;
+   static double average;
+   static double S;
+   static double[] NP = new double[1254]; 
+   static int C;
+   static double Klassific;
+
+public static void main(String args[])
+   {
 	openFile("res/UX.txt");
 	readFile();
 	//out();
 	writeFile("res/UX_classif.txt");
-	logar();
-	
+	prubytkovosti();
+	averag();
+	standotkl();
+	interval();
+	klassif();
+
     }
-public static void logar() {
-for(int col=1;col<N;col++){
-           a=Math.log(m[col]);
-}
-System.out.print(a);
-}
+
+public static void prubytkovosti() 
+	{
+     for (int col=0;col<N-1;col++)
+     g[col]=Math.log(m[col+1])-Math.log(m[col]);
+//System.out.print(g[col]);
+	}
+
+public static void averag() 
+	{
+     for (int col=0;col<N-2;col++)
+     h = g[col]+g[col+1];
+     average=(h/(N));
+//System.out.print(average);
+	}
+
+public static void standotkl() 
+	{
+     for (int col=0;col<N-2;col++)
+     j = (m[col]-average)+(m[col+1]-average);
+     k = (Math.pow(j,2))/(N);
+     S=Math.sqrt(k);
+//System.out.print(S);
+	}
+
+public static void normalizPrub() 
+	{
+     for (int col=0;col<N-2;col++)
+     NP[col] = (g[col]-average)/S;
+//System.out.print(NP[col]);
+	}
+
+public static void interval()
+	{
+C = Integer.parseInt(JOptionPane.showInputDialog("Введіть кількість інтервалів поділу"));
+//System.out.print(name);
+	}
+
+public static void klassif() 
+	{
+     for (int col=0;col<N-2;col++)
+     Klassific =(int) Math.floor((C-1)*(NP[col]+3)/6);
+//System.out.print(Klassific);
+	}
 }
